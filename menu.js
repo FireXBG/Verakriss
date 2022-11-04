53b3896167bcd4221bb5724a5cc0ee0c77766163
FireXBG <55113543+FireXBG@users.noreply.github.com>
2022-11-04 20:56:58 +0200

    Initial commit

---
+++ menu.js
@@ -0,0 +1,14 @@
+    function changeClass() {
+    
+        document.querySelector(".burger").className = "active";
+        document.querySelector(".inactive").className = "activated"
+        document.querySelector(".navMenu").className = "open";
+    }
+    
+    function returnClass() {
+        document.querySelector(".active").className = "burger";
+        document.querySelector(".activated").className = "inactive";
+        document.querySelector(".open").className = "navMenu";
+    }
+
+    
