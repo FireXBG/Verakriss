066e9c5aa843916067966a33acf2b2c269b822c4
FireXBG <55113543+FireXBG@users.noreply.github.com>
2024-03-03 21:21:54 +0200

    Adding pages

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,4 +4,20 @@ router.get("/", (req, res) => {
   res.render("home");
 });
 
+router.get("/about", (req, res) => {
+  res.render("about");
+});
+
+router.get("/services", (req, res) => {
+  res.render("services");
+});
+
+router.get("/partners", (req, res) => {
+  res.render("partners");
+});
+
+router.get("/contacts", (req, res) => {
+  res.render("contacts");
+});
+
 module.exports = router;
--- routes.js
+++ routes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const homeController = require("./controllers/homeController");
+const newsController = require("./controllers/newsController");
 
 router.use(homeController);
+router.use("/news", newsController);
 
 module.exports = router;
